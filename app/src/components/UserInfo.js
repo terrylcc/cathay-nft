e706625be0793f5d1de70529ce437220cb50be57
Terry Leung <59078445+terrylcc@users.noreply.github.com>
2022-11-20 12:20:29 +0800

    Change membership id

--- app/src/components/UserInfo.js
+++ app/src/components/UserInfo.js
@@ -7,7 +7,7 @@ class UserInfo extends React.Component {
       <div className="userinfo">
         <ul>
             <li className='name'>Kenneth L. Hutchinson</li>
-            <li className='membership'>Diamond 2124 32 423</li>
+            <li className='membership'>Diamond 212 4324 231</li>
             <li className='edit'>Edit profile</li>
         </ul>
         <img className="logo" src={process.env.PUBLIC_URL + '/images/icons/logo.png'} alt="logo" />
